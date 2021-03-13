6b2a7613299d348422ab00ecc738054f2a3125a6
Valerio Mascagna <valerio.mascagna@pv.infn.it>
2021-03-13 11:40:09 +0100

    modified compile.sh script to create the bin folder

--- compile.sh
+++ compile.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+if [ ! -d bin ]; then
+  mkdir bin;
+fi
+
 gfortran src/antiprova.for -o bin/antip 2> /dev/null  ## suppressing a fortran compiling warning
 g++ src/coulomb.cpp -o bin/scattering
 echo "antip and scattering programs compiled (executable files in the bin directory)"
